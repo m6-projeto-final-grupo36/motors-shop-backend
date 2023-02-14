8c190fa727f9e13585e782a904d5ec364a63578c
unknown <guilhermekhirata@hotmail.com>
2023-02-14 11:45:04 -0300

    feat: criação da estrutura do projeto e adição das dependências necessárias

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata"
+import express from "express"
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(3000, () => {
+    console.log("Server running in port 3000")
+})
