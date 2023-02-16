f03f9a7de6882ba317d18bed0c139becf8a7ec02
richzago <rich.czago@gmail.com>
2023-02-16 16:11:07 -0300

    feat: adicionado o env nas config de data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,25 @@
 import { DataSource } from "typeorm";
-import "dotenv/config"
+import "dotenv/config";
 
 const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "52564gui",
-    database: "motors_shop_db",
-    synchronize: false,
-    logging: true,
-    entities: ["src/entities/*.ts"],
-    migrations: ["src/migrations/*.ts"]
-})
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: 5432,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  logging: true,
+  entities: ["src/entities/*.ts"],
+  migrations: ["src/migrations/*.ts"],
+});
 
-AppDataSource.initialize().then(() => {
-    console.log("Database connected")
-}).catch((error) => {
-    console.log(error)
-})
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-export default AppDataSource
+export default AppDataSource;
