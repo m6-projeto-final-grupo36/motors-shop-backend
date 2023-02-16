6beee73816935510c7806c51ed27a94f826454d9
richzago <rich.czago@gmail.com>
2023-02-16 10:21:40 -0300

    feat: Criado entidades de anunciantes

---
+++ src/entities/advertiser.entity.ts
@@ -0,0 +1,50 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinColumn,
+} from "typeorm";
+
+@Entity("advertisers")
+export class Advertiser {
+  @PrimaryGeneratedColumn("uuid")
+  readonly id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ length: 4 })
+  year: string;
+
+  @Column()
+  mileage: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  description: string;
+
+  @Column()
+  img_cape: string;
+
+  @Column({ nullable: true })
+  images: string[];
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ default: "car" })
+  type_vehicle: string;
+
+  @Column({ default: "sales" })
+  type: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
