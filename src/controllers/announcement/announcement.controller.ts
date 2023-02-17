adbcf5b15e7084120050976b2e21a8ad6c814e42
Jorge Kimura <103144132+jorgekimura2001@users.noreply.github.com>
2023-02-17 16:27:02 -0300

    Update announcement.controller.ts

--- src/controllers/announcement/announcement.controller.ts
+++ src/controllers/announcement/announcement.controller.ts
@@ -1,22 +1,22 @@
 import { instanceToPlain } from "class-transformer";
 import { Request, Response } from "express";
 import { Announcement } from "../../entities/announcement.entity";
-import { createAdvertiserService } from "../../services/announcement/createAnnouncement.service";
-import { listAdvertisementsService } from "../../services/announcement/listAdvertiser.service";
+import { createAnnouncementService } from "../../services/announcement/createAnnouncement.service";
+import { listAnnouncementsService } from "../../services/announcement/listAnnouncement.service";
 
 const createAnnouncementController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
   const announcementData = await req.body;
-  const announcement: Announcement = await createAdvertiserService(announcementData);
+  const announcement: Announcement = await createAnnouncementService(announcementData);
   return res.status(201).send(instanceToPlain(announcement));
 };
 
-const listAdvertisementController = async (req: Request, res: Response) => {
-  const advertisements = await listAdvertisementsService();
+const listAnnouncementController = async (req: Request, res: Response) => {
+  const announcements = await listAnnouncementsService();
 
-  return res.json(advertisements);
+  return res.json(announcements);
 };
 
-export { createAnnouncementController, listAdvertisementController };
+export { createAnnouncementController, listAnnouncementController };
