dfef055e5b3de965607c3b1af099631e88424712
Jorge Kimura <kimurajorge2001@gmail.com>
2023-02-27 20:42:03 -0300

    feat: middleware de autenticacao e se o usuario e anunciante

--- src/routes/announcement.routes.ts
+++ src/routes/announcement.routes.ts
@@ -6,6 +6,8 @@ import {
   updateAnnouncementController,
   listRetrieveAnnouncementController,
 } from "../controllers/announcement/announcement.controller";
+import { ensureAuthMiddleware } from "../middlewares/ensureAuth.middleware";
+import { ensureIsAdvertiserUserMiddleware } from "../middlewares/ensureIsAdvertiserUser.middleware";
 import { validateSchema } from "../middlewares/validateSchema.middleware";
 import {
   createAnnouncementValidator,
@@ -17,13 +19,20 @@ export const announcementRouter = Router();
 announcementRouter.post(
   "",
   validateSchema(createAnnouncementValidator),
+  ensureAuthMiddleware,
+  ensureIsAdvertiserUserMiddleware,
   createAnnouncementController
 );
 announcementRouter.get("", listAnnouncementsController);
-announcementRouter.get('/:id', listRetrieveAnnouncementController)
+announcementRouter.get("/:id", listRetrieveAnnouncementController);
 announcementRouter.patch(
   "/:id",
   validateSchema(updateAnnouncementValidator),
   updateAnnouncementController
 );
-announcementRouter.delete("/:id", deleteAnnouncementController);
+announcementRouter.delete(
+  "/:id",
+  ensureAuthMiddleware,
+  ensureIsAdvertiserUserMiddleware,
+  deleteAnnouncementController
+);
