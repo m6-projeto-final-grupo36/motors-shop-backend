7d638a5a9a20872aa0ef995db9619ba35dc2feee
Makson <maksonsantoss4@gmail.com>
2023-02-27 13:07:38 -0300

    fix: alterando retorno da rota get announcement/:id

--- src/services/announcement/listRetrieveAnnouncement.service.ts
+++ src/services/announcement/listRetrieveAnnouncement.service.ts
@@ -1,22 +1,29 @@
-import AppDataSource from "../../data-source"
-import { Announcement } from "../../entities/announcement.entity"
-import { AppError } from "../../errors/appError"
+import AppDataSource from "../../data-source";
+import { Announcement } from "../../entities/announcement.entity";
+import { AppError } from "../../errors/appError";
 
-export const listRetrieveAnnouncementService = async(idParam: string) => {
-    const announcementsRepository = AppDataSource.getRepository(Announcement)
+export const listRetrieveAnnouncementService = async (idParam: string) => {
+  const announcementsRepository = AppDataSource.getRepository(Announcement);
 
-    const announcement = await announcementsRepository.findOne({
-        where: {
-            id: idParam
-        },
-        relations: {
-            images: true
-        }
-    })
+  const announcement = await announcementsRepository.findOne({
+    where: {
+      id: idParam,
+    },
+    relations: {
+      images: true,
+    },
+  });
 
-    if(!announcement){
-        throw new AppError("Announcement not found", 404);
-    }
+  if (!announcement) {
+    throw new AppError("Announcement not found", 404);
+  }
 
-    return announcement
-}
+  const response: any = { ...announcement };
+
+  if (announcement.images.length) {
+    const images = announcement.images.map((image) => image.img);
+    response.images = images;
+  }
+
+  return response;
+};
